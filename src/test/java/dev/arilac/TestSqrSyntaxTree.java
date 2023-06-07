ed3fd5ce88670cdecbed6f8868b52148e77b530b
Ari Lacanienta <36548615+AriLacanienta@users.noreply.github.com>
2023-06-07 15:57:03 -0600

    configure junit5 jupiter

--- src/test/java/dev/arilac/TestSqrSyntaxTree.java
+++ src/test/java/dev/arilac/TestSqrSyntaxTree.java
@@ -1,25 +1,57 @@
 package dev.arilac;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.FileReader;
-import java.nio.charset.Charset;
+import java.io.IOException;
 
-import org.eclipse.lsp4j.TextDocumentItem;
-import org.junit.Test;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import dev.arilac.server.SqrSyntaxTree;
+import dev.arilac.sqrparsing.SqrLexerGrammar;
+import dev.arilac.sqrparsing.SqrParserGrammar;
+import dev.arilac.sqrparsing.SqrParserGrammar.ProgramContext;
 
 /**
  * TestSqrSyntaxTree
  */
 public class TestSqrSyntaxTree {
 
+    private String testFilepath;
+
+    @BeforeEach
+    public void SetUp() {
+        testFilepath = "C:\\Users\\arire\\Repos\\sqrls\\sqr-language-server\\src\\test\\java\\dev\\arilac\\testfile.sqr";
+    }
+
     @Test
-    public void testLoadsFile(){
-        SqrSyntaxTree testTree = new SqrSyntaxTree("C:\\Users\\arire\\Repos\\sqrls\\sqr-language-server\\src\\test\\java\\dev\\arilac\\testfile.sqr");
+    public void testSyntaxTree(){
+        SqrSyntaxTree testTree = new SqrSyntaxTree(testFilepath);
         assertTrue(true);
+    }
+
+    @Test
+    public void testLoadFile(){
+        File sourceFile = new File(testFilepath);
+        try (FileReader reader = new FileReader(sourceFile)) {
+            CharStream sourceText = CharStreams.fromReader(reader);
+
+            SqrLexerGrammar lexer = new SqrLexerGrammar(sourceText);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            SqrParserGrammar parser = new SqrParserGrammar(tokens);
+
+            ProgramContext result = parser.program();
+            
+            System.out.println(result.toStringTree());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
 }
